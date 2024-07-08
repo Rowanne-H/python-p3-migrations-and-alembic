ebb6b3e6547beabe93b116b4b762f65b2a475663
YUERONG HUANG <rowanne.h@hotmail.com>
2024-07-08 23:52:09 +1000

    complete lab

---
+++ lib/migrations/versions/6fb175cd3ab8_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: 6fb175cd3ab8
+Revises: 
+Create Date: 2024-07-08 23:40:10.254950
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '6fb175cd3ab8'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
